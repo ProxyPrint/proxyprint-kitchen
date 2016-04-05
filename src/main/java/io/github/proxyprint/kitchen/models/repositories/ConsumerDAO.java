262aa4af707bc617a4748ee890aaf591b66bb290
danielcaldas <danielcaldas@sapo.pt>
2016-04-05 01:18:22 +0100

    Basic consumer set up. NetBeans configuration file removed from repo.

---
+++ src/main/java/io/github/proxyprint/kitchen/models/repositories/ConsumerDAO.java
@@ -0,0 +1,16 @@
+package io.github.proxyprint.kitchen.models.repositories;
+
+import io.github.proxyprint.kitchen.models.consumer.Consumer;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by daniel on 04-04-2016.
+ */
+@Transactional
+public interface ConsumerDAO extends CrudRepository<Consumer,Long> {
+
+    @Transactional
+    public Consumer findByUsername(String username);
+}
