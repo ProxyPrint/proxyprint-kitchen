d781e2d39d3b03702a3a38c20370be2864b14238
José Sousa <josesousa9000@gmail.com>
2016-05-04 19:43:55 +0100

    cors for heroku-bar

--- src/main/java/io/github/proxyprint/kitchen/WebAppConfig.java
+++ src/main/java/io/github/proxyprint/kitchen/WebAppConfig.java
@@ -26,9 +26,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -54,19 +54,21 @@ public class WebAppConfig {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Add REST allowed endpoints...
-                registry.addMapping("/**").allowedOrigins("http://localhost:9000")
-                        .allowedMethods("POST","GET","DELETE","PUT");
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
-    
+
     @Bean
-    public Gson gson(){
+    public Gson gson() {
         return new GsonBuilder().setExclusionStrategies(new AnnotationExclusionStrategy()).create();
     }
 }
