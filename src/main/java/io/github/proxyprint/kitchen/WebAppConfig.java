2264da20e6aad88845e5dc03448cf35cac059b25
danielcaldas <danielcaldas@sapo.pt>
2016-05-04 17:24:03 +0100

    Integrated Swagger2 in project.

--- src/main/java/io/github/proxyprint/kitchen/WebAppConfig.java
+++ src/main/java/io/github/proxyprint/kitchen/WebAppConfig.java
@@ -29,11 +29,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableCaching
+@EnableSwagger2
 public class WebAppConfig {
 
     public static void main(String[] args) {
@@ -68,5 +74,14 @@ public class WebAppConfig {
     @Bean
     public Gson gson(){
         return new GsonBuilder().setExclusionStrategies(new AnnotationExclusionStrategy()).create();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
