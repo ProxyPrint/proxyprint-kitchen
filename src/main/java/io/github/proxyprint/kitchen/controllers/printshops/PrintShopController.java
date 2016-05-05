b522e4167a0eb1f652694f70d9e37630e60a417d
MarceloRGonc <marcelo.rgonc@gmail.com>
2016-05-05 18:19:02 +0100

    Change status printrequest.

--- src/main/java/io/github/proxyprint/kitchen/controllers/printshops/PrintShopController.java
+++ src/main/java/io/github/proxyprint/kitchen/controllers/printshops/PrintShopController.java
@@ -145,4 +145,30 @@ public class PrintShopController {
 
         return GSON.toJson(response);
     }
+
+    @Secured({"ROLE_MANAGER", "ROLE_EMPLOYEE"})
+    @RequestMapping(value = "/printshops/requests/{id}", method = RequestMethod.POST)
+    public String changeStatusPrintShopRequests(@PathVariable(value = "id") long id) {
+        JsonObject response = new JsonObject();
+        //PrintShop printshop = printshops.findOne(id);
+        PrintShop printshop = printshops.findAll().iterator().next();
+
+        if (printshop == null) {
+            response.addProperty("success", false);
+            return GSON.toJson(response);
+        }
+
+        PrintRequest printRequest = printrequests.findOne(id);
+
+        if (printRequest.getStatus() == Status.PENDING) {
+            printRequest.setStatus(Status.IN_PROGRESS);
+        } else if (printRequest.getStatus() == Status.IN_PROGRESS) {
+            printRequest.setStatus(Status.FINISHED);
+        } else if (printRequest.getStatus() == Status.FINISHED) {
+            printRequest.setStatus(Status.LIFTED);
+        }
+        printrequests.save(printRequest);
+        response.addProperty("success", true);
+        return GSON.toJson(response);
+    }
 }
