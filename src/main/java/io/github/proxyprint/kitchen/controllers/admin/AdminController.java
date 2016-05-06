5c6590cd0bae0ef9e035448a482beecffcd905db
danielcaldas <danielcaldas@sapo.pt>
2016-05-06 17:04:33 +0100

    Add more real printshops to seed method in AdminController.

--- src/main/java/io/github/proxyprint/kitchen/controllers/admin/AdminController.java
+++ src/main/java/io/github/proxyprint/kitchen/controllers/admin/AdminController.java
@@ -80,7 +80,28 @@ public class AdminController {
 
         Admin master = new Admin("master", "1234", "danielcaldas@sapo.pt");
         Consumer joao = new Consumer("João dos Santos", "joao", "1234", "joao@gmail.com", "69", "69");
-        PrintShop printshop = new PrintShop("Video Norte", "Rua Nova de Santa Cruz", 41.5594, 8.3972, "123444378", "logo", 0);
+
+        // Printshops
+        PrintShop printshop = new PrintShop("Copy Scan", "Rua Quinta dos Órfãos 16 S. Vitor (Junto à Universidade do Minho), Braga", 41.557973, -8.398398, "123555378", "logo_1", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("SmartPrint", "Rua dos Peões, Braga", 41.557973, -8.398398, "123555378", "logo_2", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("Flash Vidius - Gomes & Santana, Lda.", "R. Manuel Silva, 20 - R/C. C.P. 4490000, Povoa de Varzim, Porto", 41.379392, -8.761458, "199111333", "logo_3", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("CopyGraphe", "R. Ramalho Ortigão C Com Pescador - lj 12. C.P. 4490678, Povoa de Varzim, Porto", 41.383314, -8.761942, "123098333", "logo_4", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("Nelson Costa Santos", "Tv. Senra, 22 - Póvoa de Varzim. C.P. 4490000, Povoa de Varzim, Porto.", 41.382070, -8.763069, "153655378", "logo_5", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("Gráfica Editora Poveira, Lda.", "R. Manuel Silva, 18. C.P. 4490657, Povoa de Varzim, Porto.", 41.379605, -8.761047, "133555378", "logo_6", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("Staples Vila do Conde", "Avenida General Humberto Delgado 2, Vila do Conde", 41.370674, -8.744176, "133555378", "logo_7", 0);
+        printShops.save(printshop);
+        printshop = new PrintShop("Staples Vila da Maia", "Rua Comendador Valentim dos Santos Dinis 570, Maia", 41.246159, -8.625225, "133555378", "logo_7", 0);
+        printShops.save(printshop);
+
+
+        printshop = new PrintShop("Video Norte", "Rua Nova de Santa Cruz", 41.5594, -8.3972, "123444378", "logo_8", 0);
+
         Manager manager = new Manager("joaquim", "1234", "Joaquim Pereira", "joaquim@gmail.com");
         Employee employee = new Employee("mafalda", "1234", "Mafalda Sofia Pinto");
         RegisterRequest registerRequest = new RegisterRequest("Ana Pinto", "danielcaldas@sapo,pt", "1234", "Rua das Cruzes n20", 43.221, 41.121, "124555321", "Printer Style", false);
