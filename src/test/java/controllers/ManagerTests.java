6fbffdf339cc98341c80f24980fb6bae0ab93225
jose <josesousa9000@gmail.com>
2016-06-28 16:55:58 +0100

    fix testes do caldas (despedido)

--- src/test/java/controllers/ManagerTests.java
+++ src/test/java/controllers/ManagerTests.java
@@ -5,7 +5,6 @@ import io.github.proxyprint.kitchen.WebAppConfig;
 import io.github.proxyprint.kitchen.models.printshops.Employee;
 import io.github.proxyprint.kitchen.models.printshops.Manager;
 import io.github.proxyprint.kitchen.models.printshops.PrintShop;
-import io.github.proxyprint.kitchen.models.printshops.RegisterRequest;
 import io.github.proxyprint.kitchen.models.repositories.AdminDAO;
 import io.github.proxyprint.kitchen.models.repositories.ManagerDAO;
 import io.github.proxyprint.kitchen.models.repositories.PrintShopDAO;
@@ -22,16 +21,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Map;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 /**
  * Created by daniel on 20-06-2016.
@@ -40,7 +39,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(WebAppConfig.class)
 @WebIntegrationTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ManagerTests {
+
+    private static boolean SEEDED = false;
+
     @Autowired
     private WebApplicationContext wac;
     @Autowired
@@ -68,18 +71,22 @@ public class ManagerTests {
         this.managers.deleteAll();
         this.printshops.deleteAll();
         this.registerRequests.deleteAll();*/
-        MvcResult mvcResult = this.mockMvc.perform(post("/admin/seed")).andReturn();
+        if (!SEEDED) {
+            SEEDED = true;
+            this.mockMvc.perform(post("/admin/seed")).andReturn();
+        }
     }
 
     @Test
-    public void addEmployee() throws Exception {
-        PrintShop pshop = printshops.findOne((long)8);
+    public void _1addEmployee() throws Exception {
+        Manager m = managers.findAll().iterator().next();
+        PrintShop pshop = m.getPrintShop();
         long id = pshop.getId();
-        Manager m = managers.findByPrintShop(pshop);
+
         Employee e = new Employee("jdc", "1234", "Daniel Caldas", pshop);
         String body = GSON.toJson(e);
 
-        MvcResult mvcResult = this.mockMvc.perform(post("/printshops/"+id+"/employees").with(httpBasic(m.getUsername(), m.getPassword()))
+        MvcResult mvcResult = this.mockMvc.perform(post("/printshops/" + id + "/employees").with(httpBasic(m.getUsername(), m.getPassword()))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(body)).andReturn();
 
@@ -89,16 +96,16 @@ public class ManagerTests {
 
         Map response = GSON.fromJson(responseText, Map.class);
 
-        assert(response.get("success").equals(true));
+        assert (response.get("success").equals(true));
     }
 
     @Test
-    public void getEmployee() throws Exception {
-        PrintShop pshop = printshops.findOne((long)8);
+    public void _2getEmployee() throws Exception {
+        Manager m = managers.findAll().iterator().next();
+        PrintShop pshop = m.getPrintShop();
         long id = pshop.getId();
-        Manager m = managers.findByPrintShop(pshop);
 
-        MvcResult mvcResult = this.mockMvc.perform(get("/printshops/"+id+"/employees").with(httpBasic(m.getUsername(), m.getPassword()))).andReturn();
+        MvcResult mvcResult = this.mockMvc.perform(get("/printshops/" + id + "/employees").with(httpBasic(m.getUsername(), m.getPassword()))).andReturn();
 
         assert (mvcResult.getResponse().getStatus() == 200);
     }
